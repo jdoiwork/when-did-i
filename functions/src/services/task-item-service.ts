9741ee383046b61e85af4dd8feceed5834be0c0c
jdoi <jdoi.work@gmail.com>
2019-10-19 01:12:51 +0900

    uid付きで保存

--- functions/src/services/task-item-service.ts
+++ functions/src/services/task-item-service.ts
@@ -6,32 +6,28 @@ import { HttpsError } from 'firebase-functions/lib/providers/https'
 require('../helpers/firebase-init').init()
 
 
-// const db = admin.firestore()
+const db = admin.firestore()
 
 export async function createItem(title : string, auth: AuthInfo) {
   try {
-    const db = admin.firestore()
-    // const result = await db.collection('hellos').doc('world').set({
-    //   text: 'hello world'
-    // })
-    // return result
     console.log("createItem::args:", {title, auth})
-    const doc = await db
+    const tasks = db
       .collection('users')
       .doc(auth.uid)
-      // .set({
-      //   uid: auth.uid,
-        
-      // })
       .collection('tasks')
-      .add({
-        title,
-        lastUpdated: Date.now(),
-      })
-  return doc
-  // await doc.update({ uid: doc.id })
-  // const snap = await doc.get()
-  // return snap.data()
+    
+    const uid = tasks.doc().id
+    const newItem = {
+      uid,
+      title,
+      lastUpdated: Date.now(),
+    }
+    await tasks.doc(uid).set(newItem)
+    
+    return newItem
+      // await doc.update({ uid: doc.id })
+      // const snap = await doc.get()
+      // return snap.data()
 
   } catch (e) {
     console.error("Exception:!!!! createItem", e)
