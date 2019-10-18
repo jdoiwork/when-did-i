059dd6e90dd0952e39446b9910e6610ce9f4047a
jdoi <jdoi.work@gmail.com>
2019-10-18 16:59:38 +0900

    columnsに is-multilineをつけると複数行表示になったので、自前で分割していた無駄コードを消した

--- src/Elm/Page/TaskList.elm
+++ src/Elm/Page/TaskList.elm
@@ -21,27 +21,26 @@ type TaskListMsg = DeleteItem Uid
 
 type alias TaskListModel =
   { items : List TaskItem
-  , splitedItems : List (List TaskItem)
   }
 
 taskListInit : TaskListModel
-taskListInit = { items = dummyTasks, splitedItems = split3 dummyTasks }
+taskListInit = { items = dummyTasks }
 
 dummyTasks : List TaskItem
 dummyTasks = 
-  [ { uid = "abc", title = "aaa 0", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 1", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 2", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 3", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 4", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 5", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 6", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 7", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 8", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 9", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 10", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 11", lastUpdated = millisToPosix 1}
-  , { uid = "edf", title = "bbb 12", lastUpdated = millisToPosix 1}
+  [ { uid = "abc-0", title = "aaa 0", lastUpdated = millisToPosix 1}
+  , { uid = "edf-1", title = "bbb 1", lastUpdated = millisToPosix 1}
+  , { uid = "edf-2", title = "bbb 2", lastUpdated = millisToPosix 1}
+  , { uid = "edf-3", title = "bbb 3", lastUpdated = millisToPosix 1}
+  , { uid = "edf-4", title = "bbb 4", lastUpdated = millisToPosix 1}
+  , { uid = "edf-5", title = "bbb 5", lastUpdated = millisToPosix 1}
+  , { uid = "edf-6", title = "bbb 6", lastUpdated = millisToPosix 1}
+  , { uid = "edf-7", title = "bbb 7", lastUpdated = millisToPosix 1}
+  , { uid = "edf-8", title = "bbb 8", lastUpdated = millisToPosix 1}
+  , { uid = "edf-9", title = "bbb 9", lastUpdated = millisToPosix 1}
+  , { uid = "edf-10", title = "bbb 10", lastUpdated = millisToPosix 1}
+  , { uid = "edf-11", title = "bbb 11", lastUpdated = millisToPosix 1}
+  , { uid = "edf-12", title = "bbb 12", lastUpdated = millisToPosix 1}
   ]
 
 taskListUpdate : TaskListModel -> TaskListMsg -> (TaskListModel, Cmd TaskListMsg)
@@ -49,30 +48,26 @@ taskListUpdate model msg =
   case msg of
     CreatedItem newItem ->
       let newItems = newItem :: model.items
-      in ({ items = newItems, splitedItems = split3 newItems}, Cmd.none)
+      in ({ items = newItems }, Cmd.none)
     _ -> (model, Cmd.none)
 
 listView : TaskListModel -> Html TaskListMsg
 listView model =
-  section [class "section"] <| List.map gridListView model.splitedItems
+  section [class "section"] <| [gridListView model.items]
 
 gridListView : List TaskItem -> Html TaskListMsg
 gridListView xs =
   div [ class "container"]
-    [div [ class "columns" ] <| List.map columnView xs]
+    [div [ class "columns is-multiline" ] <| List.map columnView xs]
   
 
 columnView : TaskItem -> Html TaskListMsg
 columnView item =
   div [ class "column is-4"] [itemView item]
 
-parentTileView : List TaskItem -> Html TaskListMsg
-parentTileView items =
-  div [class "tile is-parent is-vertical_"] <| List.map itemView items
-
 itemView : TaskItem -> Html TaskListMsg
 itemView item =
-  div [class "tile is-child box_"] [itemCardView item]
+  div [class ""] [itemCardView item]
 
 itemCardView : TaskItem -> Html TaskListMsg
 itemCardView item =
@@ -121,42 +116,3 @@ actionButton iconName attrs content =
         [ i [class "icon", class <| "ion-" ++ iconName] []]
     , span [] [ text content]
     ]
-type alias ItemTuple a = (List a, List a, List a)
-
--- split3 : List a -> ItemTuple a
--- split3 xs =
---   let len = 1 + (List.length xs) // 3
---       a = List.take len xs
---       b = List.drop len xs |> List.take len
---       c = List.drop (len * 2) xs
---   in (a, b, c)
-
-split3 : List a -> List (List a)
-split3 xs =
-  let go items a =
-        case items of
-          [] -> List.reverse a
-          _ -> go (List.drop 3 items) (List.take 3 items :: a)
-  in go xs []
-split3v : List a -> ItemTuple a
-split3v xs =
-  let seed = (Left, ([], [], []))
-      dist = \item (pos, items) -> (nextPos pos, distribute item pos items)
-      (_, (a, b, c)) = List.foldl dist seed xs
-  in (List.reverse a, List.reverse b, List.reverse c)
-
-type Pos = Left | Center | Right
-
-nextPos : Pos -> Pos
-nextPos pos =
-  case pos of
-    Left -> Center
-    Center -> Right
-    Right -> Left
-
-distribute : a -> Pos -> ItemTuple a -> ItemTuple a
-distribute item pos (a, b, c) =
-  case pos of
-    Left -> (item :: a, b, c)
-    Center -> (a, item :: b, c)
-    Right -> (a, b, item :: c)
