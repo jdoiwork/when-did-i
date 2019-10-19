cdd9c82a6c95de458a26a9267c2f03fdc28f722c
jdoi <jdoi.work@gmail.com>
2019-10-20 02:10:08 +0900

    実験用コードを別ファイルに移した

---
+++ functions/src/hello.ts
@@ -0,0 +1,42 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+
+import { CallableContext } from 'firebase-functions/lib/providers/https';
+
+// admin.initializeApp(functions.config().firebase)
+require('./helpers/firebase-init').init()
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+
+export const helloFromWeb = functions.https.onCall((data:any, context: CallableContext) : any => {
+  return {
+    data,
+    context: { auth: context.auth, instanceIdToken: context.instanceIdToken }
+  }
+})
+
+export const createHelloItem = functions.https.onRequest(async (req, res) => {
+  try {
+    const db = admin.firestore()
+
+    const result = await db.collection('hellos').doc('world').set({
+      text: 'hello world'
+    })
+    res.json({
+      ok: 'ok',
+      result
+    })
+  }
+  catch (error) {
+    res.json({
+      err: 'err',
+      error: error,
+    })
+  }
+
+})
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,4 @@
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin'
 
 import { CallableContext } from 'firebase-functions/lib/providers/https';
 
@@ -7,22 +6,8 @@ import { authorize, error403 } from './helpers/authorize'
 
 import * as TaskItemService from './services/task-item-service'
 
-// admin.initializeApp(functions.config().firebase)
 require('./helpers/firebase-init').init()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-export const helloFromWeb = functions.https.onCall((data:any, context: CallableContext) : any => {
-  return {
-    data,
-    context: { auth: context.auth, instanceIdToken: context.instanceIdToken }
-  }
-})
 
 export const ok = functions.https.onCall(async (data: any, context: CallableContext) => {
   await authorize(context)
@@ -32,27 +17,6 @@ export const ok = functions.https.onCall(async (data: any, context: CallableCont
 export const err = functions.https.onCall(async (data: any, context: CallableContext) => {
   await authorize(context)
   throw error403()
-})
-
-export const createHelloItem = functions.https.onRequest(async (req, res) => {
-  try {
-    const db = admin.firestore()
-
-    const result = await db.collection('hellos').doc('world').set({
-      text: 'hello world'
-    })
-    res.json({
-      ok: 'ok',
-      result
-    })
-  }
-  catch (error) {
-    res.json({
-      err: 'err',
-      error: error,
-    })
-  }
-
 })
 
 // -------------------------------------------------------- for Task Items
