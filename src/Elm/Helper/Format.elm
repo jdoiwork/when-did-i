8940a61784a6df70c45ab9ac0d1d89d7d6d03d30
jdoi <jdoi.work@gmail.com>
2019-10-23 23:19:11 +0900

    Posixを日付Partsに変換

--- src/Elm/Helper/Format.elm
+++ src/Elm/Helper/Format.elm
@@ -4,6 +4,9 @@ import Time exposing (Posix, Zone)
 import DateFormat as DF
 import DateFormat.Relative as DF
 
+import Time.Extra as Ex
+import Date as Ex
+
 -- yyyy-MM-dd HH:mm:ss
 tokenList : List DF.Token
 tokenList = List.concat
@@ -42,3 +45,37 @@ formatTime = DF.format tokenTimeList
 
 formatTimeRe : Posix -> Posix -> String
 formatTimeRe = DF.relativeTime
+
+type alias Parts =
+    { year : Int
+    , month : Int
+    , day : Int
+    , hour : Int
+    , minute : Int
+    , second : Int
+    , millisecond : Int
+    }
+
+posixToParts : Zone -> Posix -> Parts
+posixToParts zone posix =
+  let exp = Ex.posixToParts zone posix
+  in
+  { year = exp.year
+  , month = Ex.monthToNumber exp.month
+  , day = exp.day
+  , hour = exp.hour
+  , minute = exp.minute
+  , second = exp.second
+  , millisecond = exp.millisecond
+  }
+
+partsToPosix : Zone -> Parts -> Posix
+partsToPosix zone parts =
+  { year = parts.year
+  , month = Ex.numberToMonth parts.month
+  , day = parts.day
+  , hour = parts.hour
+  , minute = parts.minute
+  , second = parts.second
+  , millisecond = parts.millisecond
+  } |> Ex.partsToPosix zone
